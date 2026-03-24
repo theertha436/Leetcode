fcbe7324a0ae92cbc421f2e6ea10376e40cb192b
theertha436 <theerthaesvar@gmail.com>
2026-03-24 22:10:18 +0530

    Add numIdenticalPairs function to count pairs
    Implement a method to count identical pairs in a list.

---
+++ numIdenticalPairs.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        count = {}
+        pairs = 0
+        for num in nums:
+            if num in count:
+                pairs += count[num]
+                count[num] += 1
+            else:
+                count[num] = 1
+        return pairs
+        
